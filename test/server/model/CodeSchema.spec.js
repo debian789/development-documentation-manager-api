a561b6bb3a3ac47d4e701e3a089c30138ab0385e
Miguel Suescun <debian789@gmail.com>
2016-07-10 16:28:07 -0500

    Pruebas unitarias de CodeSchema

--- test/server/model/CodeSchema.spec.js
+++ test/server/model/CodeSchema.spec.js
@@ -36,7 +36,7 @@ describe('CodeSchema: models', function () {
     })
 
     // create code not error
-    it ('Should create code', function (done) {
+    it ('Should create code is_public false', function (done) {
       var code = new  CodeSchema()
 
       code.title = 'copy and paste'
@@ -55,13 +55,152 @@ describe('CodeSchema: models', function () {
 
     })
 
+    // create code not error
+    it ('Should create code is_public true', function (done) {
+      var code = new  CodeSchema()
+
+      code.title = 'copy and paste'
+      code.description = 'copy and paste code all '
+      code.code = '<H1>copy and paste</H1>'
+      code.user = userTemp._id
+      code.is_public = true
+      code.save(function(err, data) {
+        expect(err).to.equal(null)
+        expect(data.title).to.equal('copy and paste')
+        expect(data.description).to.equal('copy and paste code all ')
+        expect(data.code).to.equal('<H1>copy and paste</H1>')
+        expect(data.is_public).to.equal(true)
+        expect(data.user).to.equal(userTemp._id)
+        done()
+      })
+
+    })
+
+
 
     // query all by user
-    // query by stete (public) - true
-    // query by stete (public) - false
-    // return error is not public query
+    it ('Should query all by user', function (done) {
+
+      var code = new  CodeSchema()
+
+      code.title = 'copy and paste'
+      code.description = 'copy and paste code all '
+      code.code = '<H1>copy and paste</H1>'
+      code.user = userTemp._id
+      code.save(function(err, data) {
+        expect(err).to.equal(null)
+
+        CodeSchema.find({user:userTemp._id}, function (err, data) {
+          expect(data.length).to.equal(1)
+          done()
+        })
+
+
+      })
+
+    })
+
+
+        // query all by user
+        it ('Should query all by is_public false', function (done) {
+
+          var code = new  CodeSchema()
+
+          code.title = 'copy and paste'
+          code.description = 'copy and paste code all '
+          code.code = '<H1>copy and paste</H1>'
+          code.user = userTemp._id
+          code.save(function(err, data) {
+            expect(err).to.equal(null)
+
+            CodeSchema.find({is_public:false, user: userTemp._id}, function (err, data) {
+              expect(err).to.equal(null)
+              expect(data.length).to.equal(1)
+              expect()
+              done()
+            })
+
+
+          })
+
+        })
+
+
+        // query all by user
+        it ('Should query all by is_public true', function (done) {
+
+          var code = new  CodeSchema()
+
+          code.title = 'copy and paste'
+          code.description = 'copy and paste code all '
+          code.code = '<H1>copy and paste</H1>'
+          code.user = userTemp._id
+          code.is_public = true
+          code.save(function(err, data) {
+            expect(err).to.equal(null)
+
+            CodeSchema.find({is_public:true}, function (err, data) {
+              expect(err).to.equal(null)
+              expect(data.length).to.equal(1)
+
+              done()
+            })
+
+
+          })
+
+        })
+
+
     // edit
+    it ('should allow edit a Code', function (done) {
+      var code = new CodeSchema()
+
+      code.title = 'Hola mundo !!'
+      code.description = 'Esto es una descripcion'
+      code.user = userTemp._id
+
+      code.save(function (err, data) {
+        expect(err).to.equal(null)
+        CodeSchema.findOne({user:userTemp._id, _id: data._id }, function (err, dataFind) {
+          expect(err).to.equal(null)
+          dataFind.title = 'Buscar Archivos'
+          dataFind.description = 'nueva description'
+
+          dataFind.save(function (err, dataSave) {
+            expect(dataSave.title).to.equal('Buscar Archivos')
+            expect(dataSave.description).to.equal('nueva description')
+            expect(String(dataSave._id)).to.equal(String(data._id))
+              done()
+          })
+
+        })
+      })
+    })
+
+
     // Delete
+
+    it ('Should allow delete a Code', function (done) {
+      var code = new CodeSchema()
+
+      code.title = 'Hola mundo'
+      code.user = userTemp._id
+
+      code.save(function(err, data) {
+        expect(err).to.equal(null)
+        expect(data.title).to.equal('Hola mundo')
+        CodeSchema.findOneAndRemove({_id: data._id, user:userTemp._id}, function (err, dataDelete) {
+          CodeSchema.findOne({_id: dataDelete._id}, function (err, dataFind){
+            expect(err).to.equal(null)
+            expect(dataFind).to.equal(null)
+            done()
+          })
+        })
+      })
+
+
+    })
 
 
   })
