6d19cf556764365f5a83c55be4482d01c19c3ad5
Miguel Suescun <debian789@gmail.com>
2016-07-20 00:54:12 -0500

    Refactor de implementacion de carga en before - commandsSchema.spect

--- test/server/model/CommandsSchema.spec.js
+++ test/server/model/CommandsSchema.spec.js
@@ -8,56 +8,92 @@ var UserSchema = require('server/models/UserSchema')
 
 // libreria para testear base de datos MongoDb
 var utils = require('test/server/model/UtilDB')
+var userTemp;
+var commandTemp;
+
+
+function commandValid(user,itemCommand,callback) {
+  // Create initial commands
+  var commando = new CommandsSchema()
+
+  commando.title = 'Test prueba'
+  commando.description = 'Prueba de commando'
+  commando.user = user._id
+  commando.commandItems.push(itemCommand)
+  commando.save(function (err, datos) {
+    expect(err).to.equal(null)
+    expect(datos.title).to.equal('Test prueba')
+    expect(datos.description).to.equal('Prueba de commando')
+    commandTemp = datos
+    callback()
+  })
+}
+
+function commandItemValid(found_user, callback) {
+  // Comando Item inicial
+
+  var comandoItem = new CommandItemSchema()
+  comandoItem.command = 'ls -la'
+  comandoItem.description = 'Listar informacion'
+
+  comandoItem.save(function (err, itemData) {
+    commandValid(found_user,itemData,callback)
+  })
+}
+
+function findUser (callback) {
+  /* User has been saved */
+  UserSchema.findOne({username: 'debian789'}, function (err, found_user) {
+    expect(err).to.be.null
+    expect(found_user).to.have.property('is_active', true)
+    expect(found_user.token).to.be.undefined
+    userTemp = found_user
+    callback(found_user)
+
+  })
+}
+
+function registerUser(callback) {
+  UserSchema.register({'username': 'debian789', 'email': 'debian789@gmail.com'}, 'mypassword', function (err, userCreated) {
+    expect(err).to.be.null
+    callback(userCreated)
+  })
+}
+
+
+function RequestActivationUser(callback) {
+  registerUser(function (userCreated) {
+    /* Activation function returns the user */
+    UserSchema.requestActivation(userCreated.id, function (err, userActivated) {
+      expect(err).to.be.null
+      callback(userActivated, userCreated)
+    })
+  })
+}
+
+function activateUser(callback) {
+  RequestActivationUser(function (userActivated, userCreated) {
+
+    UserSchema.activate(userCreated.id, userActivated.token, function (err, is_active_user) {
+      expect(err).to.be.null
+      expect(is_active_user).to.have.property('is_active', true)
+      expect(is_active_user.token).to.be.undefined
+      callback()
+    })
+  })
+}
+
 
 
 describe('CommandsSchema: Model', function () {
   describe('Administration command', function () {
-    var userTemp;
-    var commandTemp;
+
     beforeEach(function (done) {
-      UserSchema.register({'username': 'debian789', 'email': 'debian789@gmail.com'}, 'mypassword', function (err, new_user) {
-        expect(err).to.be.null
-        /* Activation function returns the user */
-        UserSchema.requestActivation(new_user.id, function (err, user) {
-          expect(err).to.be.null
-            UserSchema.activate(new_user.id, user.token, function (err, is_active_user) {
-              expect(err).to.be.null
-              expect(is_active_user).to.have.property('is_active', true)
-              expect(is_active_user.token).to.be.undefined
-
-              /* User has been saved */
-              UserSchema.findOne({username: 'debian789'}, function (err, found_user) {
-                expect(err).to.be.null
-                expect(found_user).to.have.property('is_active', true)
-                expect(found_user.token).to.be.undefined
-                userTemp = found_user
-
-                // Create initial commands
-                var commando = new CommandsSchema()
-                commando.title = 'Test prueba'
-                commando.description = 'Prueba de commando'
-                commando.user = found_user._id
-
-                commando.save(function (err, datos) {
-                  expect(err).to.equal(null)
-                  expect(datos.title).to.equal('Test prueba')
-                  expect(datos.description).to.equal('Prueba de commando')
-                  commandTemp = datos
-                  done()
-
-                })
-
-
-
-
-              })
-            })
+      activateUser(function() {
+        findUser(function(dataUser) {
+          commandItemValid(dataUser, done)
         })
       })
-
-
-
-
     })
 
     // Crear un comando sin item
@@ -159,7 +195,6 @@ describe('CommandsSchema: Model', function () {
            function (cb) {
              CommandsSchema.find({}, function (err,data) {
                expect(err).to.equal(null)
-               console.log(data)
                expect(data.length).to.equal(2)
               return err ? done(err) : cb();
 
@@ -169,12 +204,20 @@ describe('CommandsSchema: Model', function () {
            function (cb) {
              CommandsSchema.find({title:'Test prueba'}, function (err,data) {
                expect(err).to.equal(null)
-               console.log(data)
                expect(data.length).to.equal(1)
-               done()
+               return err ? done(err) : cb();
              })
            },
-          //  function () {},
+           // query for description
+          function (cb) {
+            // list = parametro de busqueda
+            var parameterSearch = new RegExp('list', 'i')
+            CommandsSchema.find({description: parameterSearch}, function (err, data) {
+              expect(data.length).to.equal(1)
+              done()
+            })
+
+          },
           //  function () {},
           //  function () {},
           //  function () {},
@@ -187,9 +230,42 @@ describe('CommandsSchema: Model', function () {
        })
 
      })
-    // busqueda de comando por titulo
-    // editar un comando
+
+     // Edit command
+     it ('Should edit a command', function (done) {
+       var newTitle = 'Hola mundo !!!'
+       CommandsSchema.findOne({_id: commandTemp._id}, function(err, data) {
+         data.title = newTitle
+         data.is_public= true
+         data.is_edit = true
+         data.save(function(err, datos) {
+           expect(datos.title).to.equal(newTitle)
+           expect(datos.is_public).to.equal(true)
+           expect(datos.is_edit).to.equal(true)
+           done()
+         })
+       })
+     })
     // editar item comando
+    it ('Should edit a item command', function (done) {
+      var newTitle = 'Hola mundo !!!'
+      CommandsSchema.findOne({_id: commandTemp._id}, function(err, data) {
+        data.title = newTitle
+        data.is_public= true
+        data.is_edit = true
+        console.log(data.commandItems)
+        // deberia poder crear un item de comando previo y enlazarlo
+        // y con este asegurar que se creo, luego en la funcion de guadar, seria
+        // modificar le valor para  que retornada otro datos
+        data.save(function(err, datos) {
+          expect(datos.title).to.equal(newTitle)
+          expect(datos.is_public).to.equal(true)
+          expect(datos.is_edit).to.equal(true)
+          done()
+        })
+      })
+    })
+
     // eliminar comando
     // eliminar un item del comando
 
