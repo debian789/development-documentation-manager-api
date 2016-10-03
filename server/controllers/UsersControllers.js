92f9fe2268eb74bdb5debb89e9a5b972043bd2e4
Miguel Suescun <debian789@gmail.com>
2016-10-02 21:39:37 -0500

    Limpieza del codigo

--- server/controllers/UsersControllers.js
+++ server/controllers/UsersControllers.js
@@ -1,81 +1,79 @@
-var generic_rest = require('server/controllers/generic_rest');
+var generic_rest = require('server/controllers/generic_rest')
 var mail_utils = require('server/common/mail_utils')
-var httplike_errors = require('server/common/httplike_errors');
+var httplike_errors = require('server/common/httplike_errors')
 /* Let caller define which model use (better architecture, esier testing) */
-var usersInj = function(UserModel) {
+var usersInj = function (UserModel) {
+  var users = {}
+  users.index = function (req, res) {
+    return generic_rest.index(UserModel, req, res)
+  }
 
-    var users = {};
-    users.index = function(req, res) {
-        return generic_rest.index(UserModel, req, res);
-    }
+  users.create = function (req, res) {
+    var user = new UserModel({
+      username: req.body.username,
+      email: req.body.email
+    })
 
-    users.create = function(req, res) {
-        var user = new UserModel({
-            username: req.body.username,
-            email: req.body.email
-        });
-        UserModel.register(user, req.body.password, function(err, new_user) {
-            if (err) {
-                if (err.code) {
-                    return res.status(err.code).send(err.message);
-                }
-                var err = httplike_errors.fromMongo(err);
-                return res.status(err.code).send(err.message);
-            }
+    UserModel.register(user, req.body.password, function (err, new_user) {
+      if (err) {
+        if (err.code) {
+          return res.status(err.code).send(err.message)
+        }
+        var err = httplike_errors.fromMongo(err)
+        return res.status(err.code).send(err.message)
+      }
 
-            UserModel.requestActivation(new_user.id, function(err, user_toact) {
-                if (err) {
-                    return res.status(500);
-                }
-
-                mail_utils.sendActivationMail(user_toact, function(err, html, text) {
-                    if (err) {
-                        return res.sendStatus(500);
-                    }
-
-                    return res.send(new_user);
-                });
-            });
-        });
-    }
-
-    users.activate = function(req, res) {
-        if (!req.params.userid || !req.body.token) {
-            return res.sendStatus(400);
+      UserModel.requestActivation(new_user.id, function (err, user_toact) {
+        if (err) {
+          return res.status(500)
         }
 
-        UserModel.activate(req.params.userid, req.body.token, function(err, user) {
-            if (err) {
-                if (err.code) {
-                    return res.status(err.code).send(err.message);
-                }
-                return res.sendStatus(500);
-            }
-            return res.send(user);
-        });
+        mail_utils.sendActivationMail(user_toact, function (err, html, text) {
+          if (err) {
+            return res.sendStatus(500)
+          }
+
+          return res.send(new_user)
+        })
+      })
+    })
+  }
+
+  users.activate = function (req, res) {
+    if (!req.params.userid || !req.body.token) {
+      return res.sendStatus(400)
     }
 
-    users.get = function(req, res) {
-        return generic_rest.get(UserModel, req.params.userid, req, res);
-    }
-
-    users.delete = function(req, res) {
-        if (req.user._id != req.params.userid) {
-            return res.sendStatus(401);
+    UserModel.activate(req.params.userid, req.body.token, function (err, user) {
+      if (err) {
+        if (err.code) {
+          return res.status(err.code).send(err.message)
         }
-        return generic_rest.delete(UserModel, req.params.userid, req, res);
+        return res.sendStatus(500)
+      }
+      return res.send(user)
+    })
+  }
+
+  users.get = function (req, res) {
+    return generic_rest.get(UserModel, req.params.userid, req, res)
+  }
+
+  users.delete = function (req, res) {
+    if (req.user._id != req.params.userid) {
+      return res.sendStatus(401)
     }
+    return generic_rest.delete(UserModel, req.params.userid, req, res)
+  }
 
-    users.edit = function(req, res) {
-        if (req.user._id != req.params.userid) {
-            return res.sendStatus(401);
-        }
-        return generic_rest.edit(UserModel, req.params.userid, ["username", "email"], req, res);
+  users.edit = function (req, res) {
+    if (req.user._id != req.params.userid) {
+        return res.sendStatus(401)
     }
+    return generic_rest.edit(UserModel, req.params.userid, ['username', 'email'], req, res)
+  }
 
-    return users;
-
+  return users
 }
 
-
-module.exports = usersInj;
+module.exports = usersInj
