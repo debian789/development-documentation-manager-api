caf9e595c4b688b9b37c9cc3421033e5ccd3ff70
Miguel Suescun <debian789@gmail.com>
2016-11-07 10:07:24 -0500

    Configuracion de estilos

--- cliente/ng2-admin/src/app/pages/login/login.module.ts
+++ cliente/ng2-admin/src/app/pages/login/login.module.ts
@@ -2,6 +2,7 @@ import { NgModule }      from '@angular/core';
 import { CommonModule }  from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgaModule } from '../../theme/nga.module';
+import { AlertModule } from 'ng2-bootstrap/ng2-bootstrap';
 
 import { Login } from './login.component';
 import { routing }       from './login.routing';
@@ -13,7 +14,8 @@ import { routing }       from './login.routing';
     ReactiveFormsModule,
     FormsModule,
     NgaModule,
-    routing
+    routing,
+    AlertModule
   ],
   declarations: [
     Login,
