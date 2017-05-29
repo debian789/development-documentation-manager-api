5a2d2031739501893a84abba6da38ca1fb548537
Miguel Suescun <debian789@gmail.com>
2017-05-29 09:17:34 -0500

    Ajustes de conexion DB#

--- server/server.js
+++ server/server.js
@@ -14,10 +14,11 @@ import passport from 'passport'
 import  { DATA_BASE, SERVER } from './common/constants'
 
 // Connect mongo database
-mongoose.connect(`mongodb://${DATA_BASE.HOST_DB}:${DATA_BASE.PORT_DB}/${DATA_BASE.NAME_DB}`,{
-  user: DATA_BASE.USER_DB,
-  pass: DATA_BASE.PASS_DB
-})
+if (DATA_BASE.USER_DB && DATA_BASE.PASS_DB) {
+  mongoose.connect(`mongodb://${DATA_BASE.USER_DB}:${DATA_BASE.PASS_DB}@${DATA_BASE.HOST_DB}:${DATA_BASE.PORT_DB}/${DATA_BASE.NAME_DB}`)
+} else {
+  mongoose.connect(`mongodb://${DATA_BASE.HOST_DB}:${DATA_BASE.PORT_DB}/${DATA_BASE.NAME_DB}`)
+}
 
 const GRAPHQL_PORT = SERVER.GRAPHQL_PORT
 const WS_PORT = SERVER.WS_PORT
